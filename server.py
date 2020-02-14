2f536988c059b370524e5963119d6b66dfcdaa7c
mtlang <mtlang@comcast.net>
2020-02-13 20:15:04 -0600

    First draft of post_post method
    Inserts don't actually work, and we throw a 400 error on line 12 if the request doesn't include json.

--- server.py
+++ server.py
@@ -1,21 +1,48 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, abort, jsonify
 import sqlite3
 
 app = Flask(__name__)
 
+@app.errorhandler(400)
+def bad_post_data(error):
+    return jsonify(error=str(error)), 400
+
 @app.route('/post', methods=['POST'])
 def post_post():
-    return request.json
+    request_json = request.get_json()
+
+    print(f'Recieved post request: {request_json}')
+
+    # Input validation
+    if request_json == None:
+        abort(400, 'Request requires JSON data including a "title" and a "body".')
+
+    if "title" not in request_json and "body" not in request_json:
+        abort(400, 'Post must include a "title" and a "body".')
+    elif "title" not in request_json:
+        abort(400, 'Post must include a "title".')
+    elif "body" not in request_json:
+        abort(400, 'Post must include a "body".')
+
+    # If we get to this point, we can extract the title and body from the request
+    new_post_data = (request_json["title"], request_json["body"])
+
+    # Connect to the database
+    connection = sqlite3.connect('blog.db')
+    
+    connection.execute("INSERT INTO posts (title,body) VALUES(?,?)", new_post_data)
+
+    return "Successfully posted", 201
 
 @app.route('/posts', methods=['GET'])
 def get_posts():
-    result = [] # the return value (the response body)
+    result = [] # The return value (the response body)
 
-    # connect to the database
+    # Connect to the database
     connection = sqlite3.connect('blog.db')
     cursor = connection.cursor()
 
-    # add a dictionary to the output for each row
+    # Add a dictionary to the output for each row
     for row in cursor.execute("SELECT * FROM posts"):
         blog_post = {
             "post_id": row[0],
@@ -24,5 +51,5 @@ def get_posts():
         }
         result.append(blog_post)
 
-    # return the result
+    # Return the result
     return jsonify(result)
